8fe19940cdfde07c8d12ba0affcd575c759bfdad
abdinaa17 <abdinassir586@gmail.com>
2024-12-28 12:15:45 -0500

    first commit

---
+++ shopmart-spring-backend/src/main/java/com/example/shopmart_spring_backend/ShopmartSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopmart_spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmartSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmartSpringBackendApplication.class, args);
+	}
+
+}
---
+++ shopmart-spring-backend/src/test/java/com/example/shopmart_spring_backend/ShopmartSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopmart_spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmartSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
